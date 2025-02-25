424f087b1d8f64c8b6daf2aabf44649c342b961c
jkcachero <johnguitarizta@gmail.com>
2025-02-25 19:55:37 +0800

    Update table colors and use Fantasque Sans Nerd Font Mono font

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -3,7 +3,7 @@
 
     <div class="flex flex-col mt-4 w-full lg:flex-row">
         <!-- Earning and Expense Forms -->
-        <div class="flex lg:sticky lg:top-24 flex-col justify-start self-start p-4 mb-4 space-y-4 w-full rounded-xl lg:w-2/5 bg-slate-200 lg:mb-0 lg:mr-4">
+        <div class="flex lg:sticky lg:top-24 flex-col justify-start self-start p-4 mb-4 space-y-4 w-full rounded-2xl lg:w-2/5 bg-slate-200 lg:mb-0 lg:mr-4">
             <form wire:submit.prevent="submitEarning" class="flex flex-col p-4 space-y-4 w-full rounded-xl bg-slate-50">
                 <p class="text-lg font-bold text-green-500">Earning</p>
                 <div>
@@ -61,21 +61,24 @@
             </form>
         </div>
 
-        <div class="p-4 w-full rounded-xl lg:w-3/5 bg-slate-200">
+        <div class="p-4 w-full rounded-2xl lg:w-3/5 bg-slate-200">
             <div class="flex sticky top-24 flex-col items-center min-w-full">
                 <!-- Scrollable container for table and links -->
-                <div class="overflow-x-auto w-full">
+                <div class="{{ count($entries['data']) != 0 ? 'overflow-x-scroll w-full' : 'w-full' }} rounded-xl">
                     <!-- Budget Table -->
-                    <table class="w-full rounded-lg border-collapse table-auto zebra bg-slate-300">
+                    <table class="w-full min-w-max border-collapse table-auto bg-slate-300 font-fantasque text-sm">
                         <style>
                             .expense-row {
-                                background-color: #f2dede;
+                                background-color: rgba(242, 222, 222, 0.5);
                             }
                             .income-row {
-                                background-color: #dff0d8;
+                                background-color: rgba(223, 240, 216, 0.5);
+                            }
+                            .zebra-row:nth-child(even) {
+                                background-color: rgba(203, 213, 225, 0.1);
                             }
                         </style>
-                        <thead>
+                        <thead class="bg-slate-800 text-gray-50">
                             <tr>
                                 <th class="px-4 py-2 text-left">Date Time</th>
                                 <th class="px-4 py-2 text-left">Type</th>
@@ -86,18 +89,18 @@
                             </tr>
                         </thead>
                         <tbody>
-                            @if(count($entries) == 0)
+                            @if(count($entries['data']) == 0)
                                 <tr>
-                                    <td colspan="6" class="px-4 py-2 text-center bg-white">No entries found.</td>
+                                    <td colspan="6" class="px-4 py-2 text-center bg-white text-2xl font-bold text-slate-500">No entries found.</td>
                                 </tr>
                             @else
                                 @foreach($entries['data'] as $entry)
-                                    <tr class="{{ $entry['type'] == 'Expense' ? 'expense-row' : 'income-row' }}">
+                                    <tr class="zebra-row {{ $entry['type'] == 'Expense' ? 'expense-row' : 'income-row' }}">
                                         <td class="px-4 py-2">{{ $entry['date'] }}</td>
                                         <td class="px-4 py-2 {{ $entry['type'] == 'Expense' ? 'text-red-600' : 'text-green-600' }}">{{ $entry['type'] }}</td>
                                         <td class="px-4 py-2 font-bold order-gray-400">{{ $entry['category'] }}</td>
                                         <td class="px-4 py-2">{{ $entry['description'] }}</td>
-                                        <td class="px-4 py-2 text-right border-r border-l border-gray-400">{{ $entry['amount'] }}</td>
+                                        <td class="px-4 py-2 text-right border-r border-l border-gray-400 {{ $entry['type'] == 'Expense' ? 'text-red-600' : 'text-green-600' }}">{{ $entry['amount'] }}</td>
                                         <td class="px-4 py-2 text-right">
                                             <button wire:click="deleteEntry({{ $entry['id'] }}, '{{ $entry['type'] }}')" class="p-2 text-white bg-red-500 rounded">Delete</button>
                                         </td>
@@ -105,24 +108,24 @@
                                 @endforeach
                             @endif
                             <tr class="border-t border-black">
-                                <th colspan="4" class="px-4 py-2 font-light text-right text-green-500 bg-white">Total Earnings:</th>
-                                <th class="px-4 py-2 text-right text-green-500 bg-white border-r border-l border-gray-400">{{ $totalEarnings }}</th>
-                                <th class="bg-white"></th>
+                                <th colspan="4" class="px-4 py-2 font-light text-right text-green-300 bg-slate-800">Total Earnings:</th>
+                                <th class="px-4 py-2 text-right text-green-300 bg-slate-800 border-r border-l border-gray-400">{{ $totalEarnings }}</th>
+                                <th class="bg-slate-800"></th>
                             </tr>
                             <tr>
-                                <th colspan="4" class="px-4 py-2 font-light text-right text-red-500 bg-white">Total Expenses:</th>
-                                <th class="px-4 py-2 text-right text-red-500 bg-white border-r border-l border-gray-400">{{ $totalExpenses }}</th>
-                                <th class="bg-white"></th>
+                                <th colspan="4" class="px-4 py-2 font-light text-right text-red-300 bg-slate-800">Total Expenses:</th>
+                                <th class="px-4 py-2 text-right text-red-300 bg-slate-800 border-r border-l border-gray-400">{{ $totalExpenses }}</th>
+                                <th class="bg-slate-800"></th>
                             </tr>
                             <tr>
-                                <th colspan="4" class="px-4 py-2 font-light text-right text-white bg-blue-300">Page {{ $entries['current_page'] }} Total Balance:</th>
-                                <th class="px-4 py-2 text-right text-white bg-blue-300 border-r border-l border-white">{{ $currentPageBalace }}</th>
-                                <th class="text-white bg-blue-300"></th>
+                                <th colspan="4" class="px-4 py-2 font-light text-right text-white bg-slate-700">Page {{ $entries['current_page'] }} Total Balance:</th>
+                                <th class="px-4 py-2 text-right text-white bg-slate-700 border-r border-l border-white">{{ $currentPageBalace }}</th>
+                                <th class="bg-slate-700"></th>
                             </tr>
                             <tr>
-                                <th colspan="4" class="px-4 py-2 font-light text-right text-white bg-blue-400 rounded-bl-xl">All Pages Total Balance:</th>
-                                <th class="px-4 py-2 text-right text-white bg-blue-400 border-r border-l border-white">{{ $totalBalance }}</th>
-                                <th class="bg-blue-400 rounded-br-xl"></th>
+                                <th colspan="4" class="px-4 py-2 font-light text-right text-white bg-slate-700 rounded-bl-xl">All Pages Total Balance:</th>
+                                <th class="px-4 py-2 text-right text-white bg-slate-700 border-r border-l border-white">{{ $totalBalance }}</th>
+                                <th class="bg-slate-700 rounded-br-xl"></th>
                             </tr>
                         </tbody>
                     </table>
