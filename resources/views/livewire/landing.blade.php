2467b904c1cfb52dfde3aee9b86e36acec120393
jkcachero <johnguitarizta@gmail.com>
2025-03-02 07:22:37 +0800

    Fix welcome text

--- resources/views/livewire/landing.blade.php
+++ resources/views/livewire/landing.blade.php
@@ -1,5 +1,5 @@
 <div class="bg-slate-800 text-slate-300 flex flex-col items-center justify-center min-h-screen space-y-8 font-fantasque px-4">
-    <h1 class="text-4xl font-bold">Welcome to WalletSaga</h1>
+    <h1 class="mt-20 text-4xl font-bold text-center">Welcome to WalletSaga</h1>
 
     <livewire:financial-trend />
 </div>
