e0ea52bd187da652ef039c0ce3cbe2d2b50a5b2e
jkcachero <johnguitarizta@gmail.com>
2025-03-03 20:30:46 +0800

    Authentication pages logo

--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -1,9 +1,9 @@
-<div class="min-h-screen flex flex-col items-center justify-center sm:justify-center items-center pt-6 sm:pt-0 bg-slate-800 px-4">
-    <div>
-        <p class="text-slate-300 font-fantasque text-5xl mb-20">WalletSaga</p>
-    </div>
+<div class="min-h-screen flex flex-col items-center justify-center space-y-4 sm:justify-center pt-6 sm:pt-0 bg-slate-800 px-4">
+    <x-authentication-card-logo />
+    <p class="text-slate-300 font-fantasque text-3xl pb-20">WalletSaga</p>
 
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-slate-900 shadow-md overflow-hidden rounded-2xl">
+
+    <div class="w-full sm:max-w-md pt-6 px-6 py-4 bg-slate-900 shadow-md overflow-hidden rounded-2xl">
         {{ $slot }}
     </div>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,11 @@ use Illuminate\Support\Facades\Route;
 use App\Livewire\Dashboard;
 use App\Livewire\Landing;
 
-Route::get('/', Landing::class)->name('home');
-
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
+    Route::get('/', Landing::class)->name('home');
     Route::get('/dashboard', Dashboard::class)->name('dashboard');
 });
