16dfad87efd9e35e49280fc6ba1ff8f54fd185ab
jkcachero <johnguitarizta@gmail.com>
2025-03-03 21:09:22 +0800

    Home page logo

--- resources/views/livewire/landing.blade.php
+++ resources/views/livewire/landing.blade.php
@@ -1,5 +1,8 @@
 <div class="bg-slate-800 text-slate-300 flex flex-col items-center justify-center min-h-screen space-y-8 font-fantasque p-4">
-    <h1 class="mt-20 text-4xl font-bold text-center">Welcome to WalletSaga</h1>
+    <div class="mt-24">
+        <x-authentication-card-logo />
+    </div>
+    <h1 class="text-2xl font-bold text-center">Welcome to WalletSaga</h1>
 
     <livewire:financial-trend />
 </div>
