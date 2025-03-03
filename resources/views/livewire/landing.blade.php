277e2e3f6cb57fe16ce23f383b7446702c1ee815
jkcachero <johnguitarizta@gmail.com>
2025-03-03 12:01:10 +0800

    Landing Padding bottom for smaller screen

--- resources/views/livewire/landing.blade.php
+++ resources/views/livewire/landing.blade.php
@@ -1,4 +1,4 @@
-<div class="bg-slate-800 text-slate-300 flex flex-col items-center justify-center min-h-screen space-y-8 font-fantasque px-4">
+<div class="bg-slate-800 text-slate-300 flex flex-col items-center justify-center min-h-screen space-y-8 font-fantasque p-4">
     <h1 class="mt-20 text-4xl font-bold text-center">Welcome to WalletSaga</h1>
 
     <livewire:financial-trend />
